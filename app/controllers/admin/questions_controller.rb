12bb500022837960b06920b26057fcadf81bce0a
Samuel Giles <livelikeasam@googlemail.com>
2014-01-24 22:57:08 +0000

    Moved admin home into admin controller namespace + added create question with answer page to admin + Added mockup of user invitation page

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -6,6 +6,21 @@ class Admin::QuestionsController < Admin::AdminController
 	end
 
 	def create
+
+		@question = Question.new
+		@question.title = params[:question][:title]
+		@question.content = params[:question][:content]
+		@question.user = current_user
+		@question.save
+
+		@answer = @question.answers.build
+		@answer.content = params[:answer][:content]
+		@answer.user = current_user
+		@answer.accepted = true
+		@answer.save
+
+		redirect_to :new_admin_question, :notice => "Question + Answer successfully created"
+
 	end
 
 end
