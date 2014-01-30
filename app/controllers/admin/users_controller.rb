60843d4318e214a3a9a54d14dd61b495038d672b
Samuel Giles <livelikeasam@googlemail.com>
2014-01-30 14:04:51 +0000

    Added show method to user controller in admin that prints user in json

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,19 @@ class Admin::UsersController < Admin::AdminController
 	def add
 	end
 
-	def view
+	def index
+	end
+
+	def show
+
+		@user = User.find(params[:id])
+
+		respond_to do |format|
+
+			format.json { render json: @user }
+
+		end
+
 	end
 
 end
