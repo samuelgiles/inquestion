71f0ecde72a075bad7196d20e7eacd25d28f264c
Samuel Giles <livelikeasam@googlemail.com>
2014-01-31 14:40:47 +0000

    ActionView::Helpers::DateHelper needed to be included in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  include ActionView::Helpers::DateHelper
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
