c4db42fa8d3c2d86411d03897c67d295d732540e
Samuel Giles <livelikeasam@googlemail.com>
2014-02-03 22:19:15 +0000

    Add unseen lambda to notification model
    Allows for user.notifications.unseen

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,7 @@
 class Notification < ActiveRecord::Base
   belongs_to :user
   belongs_to :author, :class_name => "User", :foreign_key => "author_id"
+  
+  scope :unseen, lambda { where(seen: nil) }
+
 end
