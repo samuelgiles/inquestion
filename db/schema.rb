c42539a43f3c10ea9473d8d637ba64489875191c
Samuel Giles <livelikeasam@googlemail.com>
2014-02-03 21:28:42 +0000

    Added notifications to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140201211528) do
+ActiveRecord::Schema.define(version: 20140203203203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,18 @@ ActiveRecord::Schema.define(version: 20140201211528) do
 
   add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "notifications", force: true do |t|
+    t.string   "content"
+    t.string   "link"
+    t.boolean  "seen"
+    t.integer  "user_id"
+    t.integer  "author_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "notifications", ["user_id"], name: "index_notifications_on_user_id", using: :btree
 
   create_table "notifytags", force: true do |t|
     t.integer  "user_id"
