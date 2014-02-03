5fdc84cbb2daaa1f228f1eeeb039a5b2f775b50b
Samuel Giles <livelikeasam@googlemail.com>
2014-02-03 22:18:28 +0000

    Added answer notification
    Sends question author a notification that a user has answered their question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,5 @@
 class Answer < ActiveRecord::Base
+  include Rails.application.routes.url_helpers
   belongs_to :question
   belongs_to :user
   has_many :answer_votes
@@ -6,6 +7,14 @@ class Answer < ActiveRecord::Base
   def votecount
 
   	self.answer_votes.count
+
+  end
+
+  after_create :create_new_answer_notification
+  def create_new_answer_notification
+    
+  	u = self.question.user
+  	u.notifications.create(:content => "has answered your question \"#{self.question.title}\"", :author => self.user, :link => question_path(:id => self.question.id))
 
   end
 
