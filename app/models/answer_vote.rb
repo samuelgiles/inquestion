047c58e16637078ae306ec20de97f9a16ab81e31
Samuel Giles <livelikeasam@googlemail.com>
2014-02-05 21:33:52 +0000

    Added notification to notify an author of a question that a user has submitted an answer

--- app/models/answer_vote.rb
+++ app/models/answer_vote.rb
@@ -1,4 +1,12 @@
 class AnswerVote < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
+
+  after_create :create_new_answer_vote_notification
+  def create_new_answer_vote_notification
+    
+  	u = self.question.user
+  	u.notifications.create(:content => "has voted your answer to question: \"#{self.question.title}\"", :author => self.user, :link => question_path(:id => self.question.id))
+
+  end
 end
