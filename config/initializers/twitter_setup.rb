3ed45ac31f5618756970b34f3b8d4a530b840fff
Samuel Giles <livelikeasam@googlemail.com>
2014-02-10 15:10:14 +0000

    Remove twitter_setup.rb for good

--- config/initializers/twitter_setup.rb
+++
@@ -1,8 +0,0 @@
-require 'twitter'
-
-TwitterClient = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "10BeY1OXjKuHAFzuZGovQ"
-  config.consumer_secret     = "YLRY2vjlOxapKavtJWdG3aI8cVw7hRy3zZ3PmQ1gs"
-  config.access_token        = "2333926718-LLTlJG4gcmW1ed5F40X1j6eeD1hwfRshcIWabqz"
-  config.access_token_secret = "qfLaCHGju4Fj7pgMOTDG5OiP0SHLoURx8lUDQbLnf6kig"
-end
