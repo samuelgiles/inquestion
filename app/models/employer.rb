5ede32565f969dd1f88d1fcc2898aad2d127dc0a
Samuel Giles <livelikeasam@googlemail.com>
2014-02-16 23:27:08 +0000

    Validation for employer

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -2,4 +2,7 @@ class Employer < ActiveRecord::Base
 
 	has_many :apprentices, :class_name => "User", :foreign_key => "employer_id"
 
+	validates :name, :presence => true
+  	validates :address, :presence => true
+
 end
