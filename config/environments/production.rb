11fc636667106b20db8c22f9b5f2415871ddf7c8
Samuel Giles <livelikeasam@googlemail.com>
2014-03-06 20:46:50 +0000

    Using sendmail for the moment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,11 @@ Inquestion::Application.configure do
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.sendmail_settings = {
+    :location => '/usr/sbin/sendmail',
+    :arguments => '-i -t'
+  }
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
