6f94b6b29d140c00539693c30f23acd6788f19d2
Samuel Giles <livelikeasam@googlemail.com>
2014-03-06 20:53:24 +0000

    Needed to add default_url_options :host to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,8 @@ Inquestion::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => "inquestion.co.uk" }
+
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
   config.action_mailer.delivery_method = :sendmail
