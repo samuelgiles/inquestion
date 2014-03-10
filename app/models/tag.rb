b89d583bc0d9c086032542764e062cf73fdd9ce9
Samuel Giles <livelikeasam@googlemail.com>
2014-03-10 20:01:48 +0000

    Removed before_save and overwrote attribute writer for tag title to do lowercase and remove hash

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -7,11 +7,13 @@ class Tag < ActiveRecord::Base
 	has_many :subscribed_users, :foreign_key => 'user_id', :class_name => "User", :through => :notifytags, :source => :user
 	has_many :notifytags
 
+	def title=(val)
+		write_attribute(:title, val.downcase.gsub!("#", ""))
+	end
+
 	include PgSearch
 	pg_search_scope :search, :against => {
     :title => 'A'
   	}, :using => { :trigram => { :threshold => 0.1 } }
-
-	before_save { |tag| tag.title = tag.title.downcase }
 
 end
